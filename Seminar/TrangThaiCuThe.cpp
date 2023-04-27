c63e8a56e082fcd5c30cdfb54abf90af4ba84f04
Hungyen0303 <95702577+Hungyen0303@users.noreply.github.com>
2023-04-27 19:04:28 +0700

    Update TrangThaiCuThe.cpp

--- Seminar/TrangThaiCuThe.cpp
+++ Seminar/TrangThaiCuThe.cpp
@@ -43,3 +43,76 @@ void KhoiTao::thongtinTrangThai() {
 	cout << "Trang thai KhoiTao\nDon hang can co toi thieu cac thong tin SDT,"
 		<<"DIACHI nguoi nhan va SOLUONG SAN PHAM de hoan tat khoi tao\n";
 }
+
+DangGiao::DangGiao(DonHang* DH) :TrangThai(DH) {}
+void DangGiao::doiTenNguoiNhan() {
+	cout << "Nhap vao ten nguoi nhan: \n";
+	string ten;
+	cin.ignore();
+	getline(cin, ten, '\n');
+	this->dh->datTenNguoiNhan(ten);
+}
+void DangGiao::doiSDTNguoiNhan() {
+	cout << "Nhap vao sdt nguoi nhan: \n";
+	string sdt;
+	cin.ignore();
+	getline(cin, sdt, '\n');
+	this->dh->datSDTNguoiNhan(sdt);
+}
+void DangGiao::doiDiaChiNhan() {
+	cout << "Khong the doi dia chi !!!" << endl; 
+}
+void DangGiao::themSanPham() {
+	cout << "Khong the them san pham , don hang dang duoc giao !!! " << endl; 
+}
+void DangGiao::huyBoDH() {
+	dh.huyBoDH();
+}
+void DangGiao::thongtinTrangThai() {
+	cout << "Trang thai dang giao" << endl; 
+}
+HoanThanh::HoanThanh(DonHang* DH) :TrangThai(DH) {}
+void HoanThanh::doiTenNguoiNhan() {
+	cout << "Don hang da giao thanh cong" << endl; 
+}
+void HoanThanh::doiSDTNguoiNhan() {
+	cout << "Don hang da giao thanh cong" << endl;
+}
+void HoanThanh::doiDiaChiNhan() {
+	cout << "Don hang da giao thanh cong" << endl;
+}
+void HoanThanh::themSanPham() {
+	cout << "Don hang da giao thanh cong" << endl;
+}
+void HoanThanh::huyBoDH() {
+	cout << "Don hang da giao thanh cong, khong the huy bo" << endl;
+}
+void HoanThanh::thongtinTrangThai() {
+	cout << "Trang thai: Da hoan thanh " << endl;
+}
+DaHuyBo::DaHuyBo(DonHang* DH) :TrangThai(DH) {}
+void DaHuyBo::doiTenNguoiNhan() {
+	cout << "Don hang da huy bo" << endl
+		<< "moi khach hang co the dat lai don khac" << endl;
+}
+void DaHuyBo::doiSDTNguoiNhan() {
+	cout << "Don hang da huy bo"
+		<< "moi khach hang co the dat lai don khac" << endl;
+}
+void DaHuyBo::doiDiaChiNhan() {
+	cout << "Don hang da huy bo" 
+		<< "moi khach hang co the dat lai don khac" << endl;
+}
+void DaHuyBo::themSanPham() {
+	cout << "Don hang da huy bo" 
+		<< "moi khach hang co the dat lai don khac"<< endl;
+}
+void DaHuyBo::huyBoDH() {
+	cout << "Don hang da huy bo" 
+		<< "moi khach hang co the dat lai don khac" << endl;
+}
+void DaHuyBo::thongtinTrangThai() {
+	cout << "Don hang da huy bo" << endl; 
+}
+
+
