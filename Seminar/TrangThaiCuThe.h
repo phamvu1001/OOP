6832791a961a17d8456cd1f71d1b1a431749d031
Hungyen0303 <95702577+Hungyen0303@users.noreply.github.com>
2023-04-27 19:02:25 +0700

    Update TrangThaiCuThe.h

--- Seminar/TrangThaiCuThe.h
+++ Seminar/TrangThaiCuThe.h
@@ -24,4 +24,35 @@ public:
 		cout << "Xac nhan\n";
 	}
 };
+class DangGiao :public TrangThai {
+public:
+	DangGiao(DonHang* DH) :TrangThai(DH) {}
+	void doiTenNguoiNhan();
+	void doiSDTNguoiNhan();
+	void doiDiaChiNhan();
+	void themSanPham();
+	void huyBoDH();
+	void thongtinTrangThai();
+
+};
+class HoanThanh :public TrangThai {
+public:
+	HoanThanh(DonHang* DH) :TrangThai(DH) {}
+	void doiTenNguoiNhan();
+	void doiSDTNguoiNhan();
+	void doiDiaChiNhan();
+	void themSanPham();
+	void huyBoDH();
+	void thongtinTrangThai();
+};
+class DaHuyBo :public TrangThai {
+public:
+	DaHuyBo(DonHang* DH) :TrangThai(DH) {}
+	void doiTenNguoiNhan();
+	void doiSDTNguoiNhan();
+	void doiDiaChiNhan();
+	void themSanPham();
+	void huyBoDH();
+	void thongtinTrangThai();
+};
 #endif // !_TRANGTHAICUTHE_H
