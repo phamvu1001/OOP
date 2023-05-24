e09f4eba54787d09c484fef41a2e60362258597c
trieu21120347 <trieulaobao@gmail.com>
2023-05-24 18:18:22 +0700

    Update ChessPiece.h

---
+++ Chess/ChessPiece.h
@@ -0,0 +1,16 @@
+#ifndef CHESSPIECE_H
+#define CHESSPIECE_H
+
+#include <iostream>
+
+class ChessPiece
+{
+public:
+    char color;
+    ChessPiece(char color) : color(color) {}
+    virtual ~ChessPiece() {}
+    char getColor() {return this->color;}
+    virtual bool isLegalMove(int, int, int, int, ChessBoard* cb) = 0;
+};
+
+#endif
