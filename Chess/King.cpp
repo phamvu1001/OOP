8dfb8cfa340b3791a6f3795ed285404c709ea4ec
nthuanreal <95527636+nthuanreal@users.noreply.github.com>
2023-05-25 19:21:59 +0700

    Add files via upload

---
+++ Chess/King.cpp
@@ -0,0 +1,17 @@
+﻿#include "King.h"
+
+bool King::isLegalMove(int cur_x, int cur_y, int des_x, int des_y, ChessPiece* cp[8][8]) {
+    //Kiểm tra: Bước đi có hợp lệ với luật của quân cờ không?
+        int d_x = des_x - cur_x;
+        int d_y = des_y - cur_y;
+        if (d_x == 0 && d_y == 0) return false;
+        if (abs(d_x) > 1 || abs(d_y) > 1)return false; ;
+
+    //Kiểm tra: Tại đích đến có quân cờ nào cùng màu không?
+    ChessPiece* dest = cp[des_x][des_y];
+    if (dest->getColor() == this->getColor()) {
+        return false;
+    }
+
+    return true;
+}
---
+++ Chess/Knight.cpp
@@ -0,0 +1,21 @@
+﻿#include "Knight.h"
+
+bool Knight::isLegalMove(int cur_x, int cur_y, int des_x, int des_y, ChessPiece* cp[8][8]) {
+
+    //Kiểm tra: Bước đi có hợp lệ với luật của quân cờ không?
+        int d_x = abs(cur_x - des_x);
+        int d_y = abs(cur_y - des_y);
+        //
+        if ((d_x + d_y) != 3) return false;
+        else {
+            if (d_x == 3 || d_y == 3) return false;
+        }
+
+    //Kiểm tra: Tại đích đến có quân cờ nào cùng màu không?
+        ChessPiece* dest = cp[des_x][des_y];
+        if (dest->getColor() == this->getColor()) {
+            return false;
+        }
+        dest = nullptr;
+	return true;
+}
