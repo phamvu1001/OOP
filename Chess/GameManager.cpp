c6a9d93bd6a9c29876e3ea16e27824b233784b78
ltn1006 <134504975+ltn1006@users.noreply.github.com>
2023-05-31 18:45:24 +0700

    Update GameManager.cpp

--- Chess/GameManager.cpp
+++ Chess/GameManager.cpp
@@ -1,4 +1,13 @@
 #include "GameManager.h"
+#include <vector>
+
+struct PM //Possible moves
+{
+	int srow;
+	int scol;
+	int drow;
+	int dcol;
+};
 GameManager::GameManager() {
 	this->cb = new ChessBoard;
 	this->turn = 'W';
@@ -34,7 +43,9 @@ void GameManager::getPlayerInformation() {
 		return;
 		}
 	case 2: {
-
+		this->player1 = new Person("Human", 'W');
+		this->player2 = new Computer("Computer", 'B');
+		return;
 	}
 	case 3: {
 
@@ -55,6 +66,7 @@ void GameManager::changeTurn() {
 		this->turn = 'W';
 	}
 }
+
 bool GameManager::IsGameOver() {
 	if (this->cb->isInCheck('W') && this->cb->canMove('W')==0) {
 		return 1;
@@ -105,6 +117,102 @@ void GameManager::handle() {
 		} while (!LegalMove || SelfCheckMove);
 		this->Move(srcrow, srccol, desrow, descol);
 		this->changeTurn();
+		system("cls");
+	} while (!this->IsGameOver());
+	this->cb->Print();
+	Player* winner;
+	if (this->turn == this->player1->getColor()) {
+		winner = this->player2;
+	}
+	else {
+		winner = this->player1;
+	}
+	cout << "The winner is " << winner->getName() << endl;
+}
+
+void GameManager::vsComputer() {
+	this->getPlayerInformation();
+	system("cls");
+	do {
+
+		bool LegalMove, SelfCheckMove;
+
+		int srccol, srcrow, descol, desrow;
+
+		//human
+		this->cb->Print();
+
+		do {
+			Player* player = this->getPlayerInTurn();
+			cout << player->getName() << " (" << player->getColor() << ") " << "'s turn: \n";
+			player->selectChessPiece(this->cb, srcrow, srccol);
+			player->selectDest(this->cb, srcrow, srccol, desrow, descol);
+			LegalMove = this->cb->cp[srcrow][srccol]->isLegalMove(srcrow, srccol, desrow, descol, this->cb->cp);
+			SelfCheckMove = this->IsSelfCheckMove(srcrow, srccol, desrow, descol);
+			if (!LegalMove) {
+				cout << "Not a legal move!\n";
+			}
+			if (SelfCheckMove) {
+				cout << "This move make your king in check!\n";
+			}
+		} while (!LegalMove || SelfCheckMove);
+		this->Move(srcrow, srccol, desrow, descol);
+		this->changeTurn();
+		system("cls");
+
+		//computer
+		this->cb->Print();
+
+		cout << "Computer's turn: \n";
+		LegalMove = false;
+			Player* player = this->getPlayerInTurn();
+			vector <PM> pm;
+			
+			for (int i = 7; i > 0; i--)
+			{
+
+				for (int j = 7; j > 0; j--)
+				{
+					if (cb->cp[i][j] == 0)
+					{
+						continue;
+					}
+					ChessPiece* currentPiece = cb->cp[i][j];
+					if (currentPiece->getColor() == 'B' && currentPiece != 0) {
+						for (int k = 7; k > 0; k--)
+						{
+							for (int l = 7; l > 0; l--)
+							{
+								ChessPiece* dest = cb->cp[k][l];
+								LegalMove = this->cb->cp[i][j]->isLegalMove(i, j, k, l, this->cb->cp);
+								SelfCheckMove = this->IsSelfCheckMove(i, j, k, l);
+								if (LegalMove && !SelfCheckMove)
+								{
+									PM a;
+									a.srow = i;
+									a.scol = j;
+									a.drow = k;
+									a.dcol = l;
+									pm.push_back(a);
+								}
+							}
+
+						}
+					}
+
+				}
+			}
+		if (pm.size() == 0)
+		{
+			system("cls");
+			break;
+		}
+		int random = rand() % pm.size();
+		PM m = pm[random];
+
+		this->Move(m.srow, m.scol, m.drow, m.dcol);
+		this->changeTurn();
+		pm.clear();
 		system("cls");
 	} while (!this->IsGameOver());
 	this->cb->Print();
